fb649a3ae495a864757162fcd908a74c7cc20b6c
Tomas <tomascorvalan14@gmail.com>
2024-08-10 22:22:43 -0400

    cambios menores

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,45 +1,42 @@
 const LogIn = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmpassword, setConfirmPassword] = useState("");
+  const [usuario, setUsuario] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const validarLogin = (e) => 
+    e.prevent.Default();
+    alert("funca");
+  return (
+    <div className={`wrapper modal ${openFormulario ? "is-open" : ""}`}>
+      <form action="" onSubmit={validarLogin}>
+        <h1>Log In</h1>
+        <button type="button" className="modal-close">
+          &times;
+        </button>
+        <div className="input-box">
+          <input
+            type="email"
+            placeholder="Correo"
+            required
+            onChange={(event) => setUsuario(event.target.value)}
+          />
+        </div>
+        <div className="input-box">
+          <input
+            type="password"
+            placeholder="Contraseña"
+            required
+            onChange={(event) => setContraseña(event.target.value)}
+          />
+        </div>
+        <button type="submit" className="registro">
+          Iniciar Sesion
+        </button>
+        <div className="account-exist">
+          <label>¿Olvidaste tu Contraseña?</label>
+          <a href="#">Recuerdame</a>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-    return (
-      <div className={`wrapper modal ${openFormulario ? "is-open" : ""}`}>
-        <form action="">
-          <h1>Log In</h1>
-          <button
-            type="button"
-            className="modal-close"
-          >
-             &times;
-          </button>
-          <div className="input-box">
-            <input
-              type="email"
-              placeholder="Correo"
-              required
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </div>
-          <div className="input-box">
-            <input
-              type="password"
-              placeholder="Contraseña"
-              required
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </div>
-          <button type="submit"  className="registro">
-            Iniciar Sesion
-          </button>
-          <div className="account-exist">
-            <label>¿Olvidaste tu Contraseña?</label>
-            <a href="#">Recuerdame</a>
-          </div>
-        </form>
-      </div>
-    );
-  };
-
-
-export default LogIn
+export default LogIn;
